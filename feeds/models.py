2ece610f05bee1e67ec65056efc8b80ea86b1829
Adi Krishnan <aditya.k2190@gmail.com>
2020-11-27 11:26:43 +0530

    Updated models

--- feeds/models.py
+++ feeds/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class FeedSummary(models.Model):
+    id = models.UUIDField(primary_key=True)
+    title = models.TextField()
+    link = models.URLField()
+    description = models.TextField()
+    published = models.DateTimeField()
+    source = models.CharField(max_length=200)
+
+
+class FeedDetail(models.Model):
+    id = models.UUIDField(primary_key=True)
+    post = models.TextField(null=True, blank=True)
+    feed_summary = models.ForeignKey('FeedSummary', on_delete=models.DO_NOTHING)
