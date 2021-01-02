bc924b8e645bcb767e50051d9e13e01cc566ffce
Adi Krishnan <aditya.k2190@gmail.com>
2021-01-02 22:10:56 +0530

    Added root page

--- feeds/urls.py
+++ feeds/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 
 urlpatterns = [
+    path('', views.root),
     path('quint/', views.quint_reader_view),
     path('gwtj/', views.gwtj_reader_view),
     path('moneycontrol/', views.moneycontrol_reader_view),
