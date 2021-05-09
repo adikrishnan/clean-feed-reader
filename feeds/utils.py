4a1d7d12e8c18bf5d818884f8badea67e9feea14
Adi Krishnan <aditya.k2190@gmail.com>
2021-05-09 16:12:36 +0530

    Fixed feed expires environ bug

--- feeds/utils.py
+++ feeds/utils.py
@@ -1,5 +1,6 @@
 import os
 from datetime import timedelta
+from django.conf import settings
 from django.utils.timezone import now
 from django.utils.log import logging
 from feeds.reader import FeedReader
@@ -12,13 +13,20 @@ def feed_refresh():
     """ Util that refreshes news feeds for all the sources. """
     sources = FeedSource.objects.all()
     list(map(lambda x: FeedReader(x.feed_url).posts(), sources))
-    logger.info(f'Feed refresh complete')
+    logger.info('Feed refresh complete')
 
 
 def remove_old_entries():
     """ Util that removes old entries. """
-    expire_days = os.getenv('FEED_EXPIRE_DAYS', 2)
+    try:
+        expire_days = int(os.getenv('FEED_EXPIRE_DAYS'))
+    except ValueError:
+        logger.info(
+            f'Failed to set feed expiry,'
+            f' defaulting to {settings.DEFAULT_FEED_EXPIRES_DAYS} days'
+        )
+        expire_days = settings.DEFAULT_FEED_EXPIRES_DAYS
     diff_date = now() - timedelta(days=expire_days)
-    count, model_count = FeedEntry.objects.filter(published__lte=diff_date).delete()
-    logger.info(f'Total objects deleted: {count}')
-    logger.info(f'Deleted objects per model: {model_count}')
+    c, mc = FeedEntry.objects.filter(published__lte=diff_date).delete()
+    logger.info(f'Total objects deleted: {c}')
+    logger.info(f'Deleted objects per model: {mc}')
